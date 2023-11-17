4cd57c065aed173be302cc25a4260386f6d17341
love-sonkar <loveboss806@gmail.com>
2023-11-17 16:49:24 +0530

    added disabled feature in buttons

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const ButtonComponent = ({children,onClick =()=>{},type, ...props}) => {
+const ButtonComponent = ({children,onClick =()=>{},type, disabled,...props}) => {
+  const cursorDisableCss = disabled ? "cursor-not-allowed" : ""
   return (
-    <button  {...props} type={type} onClick={onClick}  className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer'>{children}</button>
+    <button  {...props} disabled={disabled} type={type} onClick={onClick}  className={`${cursorDisableCss} text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer`}>{children}</button>
   )
 }
 
