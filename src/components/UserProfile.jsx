fb9a74a06e31799c5078f1d7ad7b86e6b4e7000d
love-sonkar <loveboss806@gmail.com>
2023-11-29 19:16:15 +0530

    added styling and section for profile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { FormSectionWrapper, ButtonComponent } from "./index.js";
+import { FormSectionWrapper } from "./index.js";
 import { useSelector } from "react-redux";
 
 const UserProfile = () => {
   const userData = useSelector((state) => state.userData);
 
+  const profileData = [
+    {title:"Name",content:userData?.name},
+    {title:"Email",content:userData?.email},
+
+  ]
+
   return (
-    <FormSectionWrapper className="flex-col">
-   <img
-      className="w-44 h-44 object-cover p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-      src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      alt="avatar"
-    />
- 
-      {userData?.name}
-      
+    <FormSectionWrapper className="flex-col md:flex-row gap-4">
+      <img
+        className="w-44 h-44 object-cover p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+        src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="avatar"
+      />
+  <div className="">
+    {profileData.map((itme)=>(
+      <div key={itme.title}>
+        <h2>{itme.title}</h2>
+        <h2>{itme.content}</h2>
+      </div>
+    ))}
+  </div>
     </FormSectionWrapper>
   );
 };
