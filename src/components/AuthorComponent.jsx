a9e6d8a2a61507cd245695df7d5da82d03787b2d
love-sonkar <loveboss806@gmail.com>
2023-12-04 22:24:15 +0530

    added mouseover and out efffect

--- src/components/AuthorComponent.jsx
+++ src/components/AuthorComponent.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AuthorComponent = ({name="dummy",cardData}) => {
+  const [isHover,setIsHover] = useState(false);
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHover(false);
+  };
+
+
   return (
     <div className="py-3 px-2 flex gap-2 items-center">
     <img
@@ -8,8 +18,13 @@ const AuthorComponent = ({name="dummy",cardData}) => {
       src="https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       alt="avatar"
     />
-    <div className='flex flex-col'>
-    <h2 className="capitalize text-xl leading-none dark:text-white">{name}</h2>
+    <div className='flex flex-col relative'>
+    <h2 onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="capitalize text-xl leading-none dark:text-white cursor-pointer">{name}</h2>
+    {isHover &&
+    <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}  className='absolute -top-full rounded-md z-20 left-1 h-10 w-10 bg-red-500'>
+      hi
+    </div>
+    }
     <p className='text-xs dark:text-white'>@{name}</p>
     </div>
   </div>
