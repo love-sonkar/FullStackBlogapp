1028b021ff7d2d13c73a7e1f5f9885c102e6236a
love-sonkar <loveboss806@gmail.com>
2023-12-08 19:18:01 +0530

    some typo error

--- src/components/formcomponent/SignupForm.jsx
+++ src/components/formcomponent/SignupForm.jsx
@@ -99,7 +99,7 @@ const SignupForm = () => {
         </div>
 
         <ButtonComponent type="submit" disabled={isSubmitting}>
-          Singup
+        Signup
         </ButtonComponent>
         <GuestLogin title="Singup" />
         <p className="text-center text-black dark:text-white">
