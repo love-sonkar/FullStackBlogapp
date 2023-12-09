753a6f523c169ecce2f2f4ef8d665261a60d22d2
love-sonkar <loveboss806@gmail.com>
2023-12-09 22:11:21 +0530

    remove some unwanted code

--- src/components/ReadSingleBlog.jsx
+++ src/components/ReadSingleBlog.jsx
@@ -29,9 +29,6 @@ const ReadSingleBlog = () => {
     };
     FetchSingleBlog();
   }, []);
-    const ImgSrc = ImageFilePreviewSrc(singlePost?.images)
-  console.log(ImgSrc)
-
   return (
     <div className="p-4 flex items-center justify-center h-full">
       {singlePost === null ? (
